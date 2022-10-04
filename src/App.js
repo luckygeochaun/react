93f61e14f864c37ae7404e447952e296bd9c2c7f
Timothy McManus <tmcmanus@firstfloorcreative.com>
2022-10-03 20:32:12 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './styles/index.css'
+import Employees from './components/employee'
 
 function App() {
-  return (
-    <div>
-      <h1>Hello My Web Friends</h1>
-    </div>
-  )
+  console.log('We are going to list all the emplyees here')
+  {
+    const hasAuth = true
+    return hasAuth ? (
+      <>
+        <Employees />
+        <Employees />
+        <Employees />
+        <Employees />
+        <Employees />
+        <Employees />
+      </>
+    ) : (
+      <h1>No Employees to show</h1>
+    )
+  }
 }
 
 export default App
---
+++ src/components/employee.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const employee = () => {
+  return (
+    <div>
+      <h1>This is an employee</h1>
+    </div>
+  )
+}
+
+export default employee
