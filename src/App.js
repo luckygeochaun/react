3bd019b2bf7247f6ee72d759376be70497f55ade
Timothy McManus <tmcmanus@firstfloorcreative.com>
2022-10-04 18:08:49 -0400

    Before Tailwinds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './styles/index.css'
 import Employees from './components/employee'
+import { useState } from 'react'
 
 function App() {
+  const [role, setRole] = useState('Dev')
   {
     const hasAuth = true
     return hasAuth ? (
@@ -9,12 +11,12 @@ function App() {
         <input
           type='text'
           onChange={(e) => {
-            console.log(e.target.value)
+            setRole(e.target.value)
           }}
         />
         <Employees name='Caleb' role='Intern' />
-        <Employees name='Timothy' role='Owner' />
-        <Employees name='Tara' role='Owner Wife' />
+        <Employees name='Timothy' role={role} />
+        <Employees name='Tara' />
       </>
     ) : (
       <h1>No Employees to show</h1>
