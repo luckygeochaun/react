c5af73d4bbe0b55827ca28bf776f79f8b4a43101
Timothy McManus <tmcmanus@firstfloorcreative.com>
2022-10-06 13:38:17 -0400

    added modal

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -27,14 +27,59 @@ function EditEmployee() {
           <Modal.Title>Edit Employees</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Fill out this form to edit the employee data that will display on the
-          website
+          <form id='editModal' className='w-full max-w-sm'>
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3'>
+                <label
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                  for='name'
+                >
+                  Full Name
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                  id='name'
+                  type='text'
+                  placeholder='Enter Employees Name'
+                />
+              </div>
+            </div>
+            <div className='md:flex md:items-center mb-6'>
+              <div className='md:w-1/3'>
+                <label
+                  className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                  for='role'
+                >
+                  Role
+                </label>
+              </div>
+              <div className='md:w-2/3'>
+                <input
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                  id='role'
+                  type='text'
+                  placeholder='Enter Employees Role'
+                />
+              </div>
+            </div>
+            <div className='md:flex md:items-center'>
+              <div className='md:w-1/3'></div>
+              <div className='md:w-2/3'></div>
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
-          <Button variant='primary'>Understood</Button>
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            form='editModal'
+          >
+            Update
+          </button>
         </Modal.Footer>
       </Modal>
     </>
